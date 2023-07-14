d1c88d94ab25f4570996ad7becc51995cd48116c
Jouni Männistö <jouni.mannisto@gmail.com>
2023-07-14 10:55:32 +0300

    9.22: Patientor, step3, (commit  3)

--- part9-c/src/services/patientsService.ts
+++ part9-c/src/services/patientsService.ts
@@ -1,33 +1,44 @@
 import data from '../../data/patients';
 import { v1 as uuid } from 'uuid';
 
-import { Patient, NonSensitivePatientEntry, NewPatientEntry } from '../types';
+import { Patient, NonSensitivePatientEntry, NewPatientEntry, EntryType } from '../types';
 import toNewPatientEntry from '../utils';
 
 const getEntries = (): Array<Patient> => {
     return data.map(({
-        id,
+         id,
+         ssn,
         name,
         dateOfBirth,
         gender,
         occupation,
-        ssn,
         entries
-     }) => ({
-        id,
+    }) => ({
+         id,
+         ssn,
         name,
         dateOfBirth,
-        gender: gender ,
+        gender,
         occupation,
-        ssn,
-        entries
+        entries: entries.map(({
+            id,
+
+            description,
+            date,
+            specialist,
+            type,
+        }) => ({
+            id,
+            description,
+            date,
+            specialist,
+            type: type as unknown as EntryType,
+        }))
+
     }));
 };
 
-
-
-
-const allEntries: Array<Patient> = data ;
+const allEntries = getEntries();
 
 
 const getNonSensitiveEntries = (): Array<NonSensitivePatientEntry> => {
@@ -55,8 +66,6 @@ const addNewPatient = (entry: NewPatientEntry): Patient => {
     allEntries.push(newPatientEntry);
     return newPatientEntry;
 };
-
-
 
 
 export default {
